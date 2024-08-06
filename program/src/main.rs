76d6e79d7a4d2c0e1c6076209a583c077781d888
MRain <linmrain@gmail.com>
2024-08-06 11:48:05 -0400

    bn syscall

--- program/src/main.rs
+++ program/src/main.rs
@@ -13,7 +13,8 @@
 
 #![no_main]
 
-use ark_ec::{pairing::Pairing, VariableBaseMSM};
+use ark_ec::pairing::Pairing;
+use ark_ff::{BigInt, BigInteger, PrimeField, Zero};
 use ark_serialize::{CanonicalDeserialize, CanonicalSerialize};
 use serde::{de::Error as _, ser::Error as _, Deserialize, Serialize};
 
@@ -29,12 +30,77 @@ pub fn main() {
     //     .zip(scalars.0)
     //     .map(|(p, s)| p * s)
     //     .sum();
-    let bigints: Vec<_> = scalars.0.into_iter().map(|s| s.into()).collect();
-    let result = <E as Pairing>::G1::msm_bigint(&points.0, &bigints);
+    // let bigints: Vec<_> = scalars.0.into_iter().map(|s| s.into()).collect();
+    // let result = <E as Pairing>::G1::msm_bigint(&points.0, &bigints);
+    // let result = unwrap_g1affine(&result);
+    let result = msm(&points.0, &scalars.0);
+    std::println!("{}", result);
 
     let mut bytes = vec![];
     result.serialize_uncompressed(&mut bytes).unwrap();
     sp1_zkvm::io::commit_slice(&bytes);
+}
+
+#[derive(Clone, Copy, Debug)]
+pub struct BnAffinePoint(pub BigInt<4>, pub BigInt<4>);
+
+fn wrap_g1affine(p: &G1Affine) -> BnAffinePoint {
+    BnAffinePoint(p.x.into_bigint(), p.y.into_bigint())
+}
+
+fn unwrap_g1affine(p: &BnAffinePoint) -> G1Affine {
+    G1Affine {
+        x: p.0.into(),
+        y: p.1.into(),
+        infinity: false,
+    }
+}
+
+fn msm(p: &[G1Affine], s: &[ScalarField]) -> G1Affine {
+    let mut iter = p.iter().zip(s).filter(|(_, s)| !s.is_zero());
+    let mut result = {
+        let (p, s) = iter.next().unwrap();
+        let mut p = wrap_g1affine(p);
+        bn254_double_and_add(&mut p, s);
+        p
+    };
+    iter.for_each(|(p, s)| {
+        let mut p = wrap_g1affine(p);
+        bn254_double_and_add(&mut p, s);
+        bn254_add(&mut result, &p);
+    });
+    unwrap_g1affine(&result)
+}
+
+fn bn254_add(p: &mut BnAffinePoint, q: &BnAffinePoint) {
+    sp1_zkvm::syscalls::syscall_bn254_add(
+        p as *mut _ as *mut [u32; 16],
+        q as *const _ as *const [u32; 16],
+    );
+}
+
+fn bn254_double_and_add(p: &mut BnAffinePoint, s: &ScalarField) {
+    // let mut t = *p;
+    let mut t = *p;
+    let mut b = s.into_bigint().to_bits_le().into_iter();
+    let mut q = {
+        b.by_ref().take_while(|b| !b).for_each(|_| {
+            sp1_zkvm::syscalls::syscall_bn254_double(&mut t as *mut _ as *mut [u32; 16]);
+        });
+        t
+    };
+    b.for_each(|b| {
+        // double in place
+        sp1_zkvm::syscalls::syscall_bn254_double(&mut t as *mut _ as *mut [u32; 16]);
+        if b {
+            sp1_zkvm::syscalls::syscall_bn254_add(
+                &mut q as *mut _ as *mut [u32; 16],
+                &t as *const _ as *const [u32; 16],
+            );
+        }
+    });
+
+    *p = q
 }
 
 type E = ark_bn254::Bn254;
